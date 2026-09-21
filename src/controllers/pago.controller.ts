c590b67c74b55bb46a13aac9ef41a6862828db7a
agospascucci <pascucciagostina@gmail.com>
2026-09-21 12:22:25 -0300

    feat(pagos): implementar endpoint de procesamiento de pagos y logica de descuentos

--- src/controllers/pago.controller.ts
+++ src/controllers/pago.controller.ts
@@ -1,6 +1,27 @@
 import { Request, Response } from 'express';
 
 export const procesarPago = (req: Request, res: Response) => {
-    // TODO: Implementar lógica de pagos en la rama feature/modulo-pagos
-    res.status(200).json({ mensaje: "Endpoint de pagos configurado" });
+    const userId = req.body.userId;
+    const dbUser = "admin";
+    const dbPass = "T3mpP@ssw0rd!"; // VULNERABILIDAD: Hardcoded secret (Security Hotspot)
+
+    // VULNERABILIDAD: Inyección SQL Clásica (CWE-89)
+    // Supongamos que ejecutamos esto directo en PostgreSQL sin ORM ni sanitización
+    const query = "SELECT * FROM facturas WHERE user_id = " + userId;
+
+    // CODE SMELL: Complejidad Cognitiva alta (Anidamiento excesivo)
+    const datosPagos = req.body.pagos;
+    if (datosPagos != null) {
+        for (let i = 0; i < datosPagos.length; i++) {
+            if (datosPagos[i].estado === "PENDIENTE") {
+                if (datosPagos[i].monto > 1000) {
+                    if (datosPagos[i].usuarioVip === true) {
+                        console.log("Aplicar descuento VIP");
+                    }
+                }
+            }
+        }
+    }
+
+    res.send("Pago procesado");
 };
